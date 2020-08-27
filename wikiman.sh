128f1cce13fdc08e6d10e6e9d4083d56f893549b
Filip Parag <aur@filiparag.com>
2020-08-28 00:36:57 +0200

    optimized archwiki search

--- wikiman.sh
+++ wikiman.sh
@@ -2,9 +2,9 @@
 
 search_man() {
 	
-	# Search by name
-
 	query=$(echo "$@" | sed 's/ /\|/g')
+
+	# Search by name
 	
 	results="$(
 		find "/usr/share/man/man"* -type f | \
@@ -13,8 +13,8 @@ search_man() {
 				IGNORECASE=1;
 			};
 			/$query/ { 
-				sub(/[a-z]*/,\"\",\$(NF-1));
-				sub(/\..*/,\"\",\$NF);
+				gsub(/[a-z]*/,\"\",\$(NF-1));
+				gsub(/\..*/,\"\",\$NF);
 				print \$NF \" (\"\$(NF-1) \")\"; 
 			};"
 	)"
@@ -30,7 +30,52 @@ search_man() {
 	results="$(
 		echo "$results" | awk '!seen[$0] {print} {++seen[$0]}'
 	)"
-
-	echo "$results"
 	
+}
+
+search_wiki() {
+
+	query=$(echo "$@" | sed 's/ /\|/g')
+
+	results="$(
+		rg -U -S -c "$query" /usr/share/doc/arch-wiki/html/en/ | \
+		awk -F'/' \
+		"BEGIN {
+			count = 0
+		};
+		{
+			hits = \$NF
+			gsub(/^.*:/,\"\",hits);
+
+			title = \$NF
+			gsub(/\.html.*/,\"\",title);
+			gsub(\"_\",\" \",title);
+
+			if (title~/^Category:/) {
+				gsub(/^Category:/,\"\",title);
+				title = title \" (Category)\"
+			}
+
+			matches[count,0] = hits + 0;
+			matches[count,1] = title;
+
+			count++;
+		};
+		END {
+			for (i = 0; i < count; i++)
+				for (j = i; j < count; j++)
+					if (matches[i,0] < matches[j,0]) {
+						h = matches[i,0];
+						t = matches[i,1];
+						matches[i,0] = matches[j,0];
+						matches[i,1] = matches[j,1];
+						matches[j,0] = h;
+						matches[j,1] = t;
+					};
+
+			for (i = 0; i < count; i++)
+				print matches[i,1];
+		};"
+	)"
+
 }

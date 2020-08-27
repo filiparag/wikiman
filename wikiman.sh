a54402d08a2d63ff1467b08f210f59cbb10ba23c
Filip Parag <filip@parag.rs>
2020-08-28 01:21:57 +0200

    added new TUI result picker

--- wikiman.sh
+++ wikiman.sh
@@ -1,8 +1,15 @@
 #! /bin/dash
 
+config_dir="${XDG_CONFIG_HOME:-"$HOME/.config"}/wikiman"
+cache_dir="${XDG_CONFIG_HOME:-"$HOME/.config"}/wikiman"
+
+init() {
+	mkdir -p "$config_dir" "$cache_dir"
+}
+
 search_man() {
 	
-	query=$(echo "$@" | sed 's/ /\|/g')
+	query="$(echo "$@" | sed 's/ /\|/g')"
 
 	# Search by name
 	
@@ -12,30 +19,35 @@ search_man() {
 			"BEGIN {
 				IGNORECASE=1;
 			};
-			/$query/ { 
-				gsub(/[a-z]*/,\"\",\$(NF-1));
-				gsub(/\..*/,\"\",\$NF);
-				print \$NF \" (\"\$(NF-1) \")\"; 
+			/$query/ {
+				title = \$NF
+				gsub(/\..*/,\"\",title);
+
+				section = \$(NF-1)
+				gsub(/[a-z]*/,\"\",section);
+
+				printf(\"%s (%s)\n\",title,section);
 			};"
 	)"
 
 	# Search by description
 
 	results="${results:+$results\n}$(
-		apropos -l $@ 2>/dev/null | awk '{ print $1 " " $2 };'
+		apropos -l $@ 2>/dev/null | \
+		awk '{ gsub(/\(|\)/,"",$2); printf("%s (%s)\n",$1,$2); };'
 	)"
 
 	# Remove duplicates
 
 	results="$(
-		echo "$results" | awk '!seen[$0] {print} {++seen[$0]}'
+		echo "$results" | awk '!seen[$0] {print} {++seen[$0]};'
 	)"
 	
 }
 
 search_wiki() {
 
-	query=$(echo "$@" | sed 's/ /\|/g')
+	query="$(echo "$@" | sed 's/ /\|/g')"
 
 	results="$(
 		rg -U -S -c "$query" /usr/share/doc/arch-wiki/html/en/ | \
@@ -51,6 +63,9 @@ search_wiki() {
 			gsub(/\.html.*/,\"\",title);
 			gsub(\"_\",\" \",title);
 
+			path = \$0
+			gsub(/:[0-9]+$/,\"\",path);
+
 			if (title~/^Category:/) {
 				gsub(/^Category:/,\"\",title);
 				title = title \" (Category)\"
@@ -58,6 +73,7 @@ search_wiki() {
 
 			matches[count,0] = hits + 0;
 			matches[count,1] = title;
+			matches[count,2] = path;
 
 			count++;
 		};
@@ -67,15 +83,47 @@ search_wiki() {
 					if (matches[i,0] < matches[j,0]) {
 						h = matches[i,0];
 						t = matches[i,1];
+						p = matches[i,2];
 						matches[i,0] = matches[j,0];
 						matches[i,1] = matches[j,1];
+						matches[i,2] = matches[j,2];
 						matches[j,0] = h;
 						matches[j,1] = t;
+						matches[j,2] = p;
 					};
-
+					
 			for (i = 0; i < count; i++)
-				print matches[i,1];
+				printf(\"%s\t%s\n\",matches[i,1],matches[i,2]);
 		};"
 	)"
 
 }
+
+picker_tui() {
+
+	command="$(
+	echo "$all_results" | fzf --with-nth 1 --delimiter '\t' | \
+		awk -F '\t' \
+		"{
+			if (NF==2)
+				printf(\"xdg-open %s\n\",\$2);
+			else {
+				gsub(/ .*$/,\"\",\$1);
+				printf(\"man %s\n\",\$1);
+			}
+		};"
+	)"
+
+}
+
+search_man $@
+
+all_results="$results"
+
+search_wiki $@
+
+all_results="${all_results:+$all_results\n}$results"
+
+picker_tui
+
+eval "$command"

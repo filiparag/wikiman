7fedb852826fa48cbccfcc0653ce5649b1c22d7c
Filip Parag <filip@parag.rs>
2020-09-01 00:22:58 +0200

    gentoo wiki text search

--- sources/gentoo.sh
+++ sources/gentoo.sh
@@ -98,8 +98,80 @@ search() {
 			};"
 		)"
 
-		results="$(
-			printf '%s\n%s' "$results_title" "$results_text" | awk '!seen[$0] && NF>0 {print} {++seen[$0]};'
-		)"
+	if [ "$conf_quick_search" != 'true' ]; then
+
+		results_text="$(
+			eval "rg -U -S -c '$rg_query' $path" | \
+			awk -F'/' \
+				"BEGIN {
+					IGNORECASE=1;
+					count=0;
+				};
+				{
+					hits = \$NF
+					gsub(/^.*:/,\"\",hits);
+
+					gsub(/:[0-9]+$/,\"\",\$0);
+
+					lang = \"en\"
+					if (NF-$nf) {
+						n = \$NF
+						gsub(/[a-z]{2}(-[a-z]{2})?\.html/,\"\",n);
+						if (n==\"\") {
+							lang = \$NF
+							gsub(/\.html$/,\"\",lang);
+							dec = 1;
+						} else {
+							dec = 0;
+						}
+						title = \"\"
+						for (i=$nf; i<=NF-dec; i++)
+							title = title ((i==$nf) ? \"\" : \"/\") \$i
+					} else {
+						title = \$NF;
+					}
+
+					gsub(/\.html$/,\"\",title);
+					gsub(\"_\",\" \",title);
+
+					path = \$0;
+
+					if (lang ~ $langs && title !~ $rg_ignore) {
+
+						matches[count,0] = hits;
+						matches[count,1] = title;
+						matches[count,2] = path;
+						matches[count,3] = lang;
+						count++;
+					}
+				};
+				END {
+					for (i = 0; i < count; i++)
+						for (j = i; j < count; j++)
+							if (matches[i,0] < matches[j,0]) {
+								h = matches[i,0];
+								t = matches[i,1];
+								p = matches[i,2];
+								l = matches[i,3];
+								matches[i,0] = matches[j,0];
+								matches[i,1] = matches[j,1];
+								matches[i,2] = matches[j,2];
+								matches[i,3] = matches[j,3];
+								matches[j,0] = h;
+								matches[j,1] = t;
+								matches[j,2] = p;
+								matches[j,3] = l;
+							};
+							
+					for (i = 0; i < count; i++)
+						printf(\"%s\t%s\tgentoo\t%s\n\",matches[i,1],matches[i,3],matches[i,2]);
+				};"
+			)"
+
+	fi
+
+	results="$(
+		printf '%s\n%s' "$results_title" "$results_text" | awk '!seen[$0] && NF>0 {print} {++seen[$0]};'
+	)"
 
 }

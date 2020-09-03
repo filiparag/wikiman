b5bf74c8c9293b9ca5d10d7af6bf628814211c6b
Filip Parag <filip@parag.rs>
2020-09-03 02:53:51 +0200

    tldr-pages search

--- sources/tldr.sh
+++ sources/tldr.sh
@@ -24,6 +24,8 @@ info() {
 
 setup() {
 
+	results_title=''
+	results_text=''
 
 	if ! available; then
 		echo "warning: tldr pages do not exist" 1>&2
@@ -65,9 +67,9 @@ setup() {
 
 list() {
 
-    setup || return 1
+	setup || return 1
 
-    eval "find $paths -type f -name '*.html'" 2>/dev/null | \
+	eval "find $paths -type f -name '*.html'" 2>/dev/null | \
 	awk -F '/' \
 		"BEGIN {
 			IGNORECASE=1;
@@ -76,20 +78,166 @@ list() {
 		{
 			title = \"\";
 			for (i=$nf+2; i<=NF; i++) {
-				fragment = toupper(substr(\$i,0,1))substr(\$i,2);
-				title = title ((i==$nf+2) ? \"\" : \"/\") fragment;
+				title = title ((i==$nf+2) ? \"\" : \"/\") \$i;
 			}
 
 			gsub(/\.html$/,\"\",title);
 			gsub(\"_\",\" \",title);
+			gsub(\"-\",\" \",title);
 
-            title = title \" (\" \$($nf+1) \")\"
+			title = title \" (\" \$($nf+1) \")\"
 
 			lang=\$$nf;
 			path=\$0;
 
 			print title, lang, \"$name\", path;
 		};"
+
+}
+
+search() {
+
+	setup || return 1
+
+	results_title="$(
+		eval "find $paths -type f -name '*.html'" 2>/dev/null | \
+		awk -F '/' \
+			"BEGIN {
+				IGNORECASE=1;
+				OFS=\"\t\"
+				count=0;
+			};
+			{
+				title = \"\";
+				for (i=$nf+2; i<=NF; i++) {
+					title = title ((i==$nf+2) ? \"\" : \"/\") \$i;
+				}
+
+				gsub(/\.html$/,\"\",title);
+				gsub(\"_\",\" \",title);
+				gsub(\"-\",\" \",title);
+
+				title = title \" (\" \$($nf+1) \")\"
+
+				lang=\$$nf;
+				path=\$0;
+
+				matched = title;
+				gsub(/$rg_query/,\"\",matched);
+
+				lm = length(matched)
+				gsub(\" \",\"\",matched);
+				
+				if (length(matched)==0)
+					accuracy = length(title)*100;
+				else
+					accuracy = 100-lm*100/length(title);
+
+				if (accuracy > 0 || book ~ /$rg_query/) {
+					matches[count,0] = accuracy;
+					matches[count,1] = title;
+					matches[count,2] = path;
+					matches[count,3] = lang;
+					count++;
+				}
+			};
+			END {
+				for (i = 0; i < count; i++)
+					for (j = i; j < count; j++)
+						if (matches[i,0] < matches[j,0]) {
+							h = matches[i,0];
+							t = matches[i,1];
+							p = matches[i,2];
+							l = matches[i,3];
+							matches[i,0] = matches[j,0];
+							matches[i,1] = matches[j,1];
+							matches[i,2] = matches[j,2];
+							matches[i,3] = matches[j,3];
+							matches[j,0] = h;
+							matches[j,1] = t;
+							matches[j,2] = p;
+							matches[j,3] = l;
+						};
+						
+				for (i = 0; i < count; i++)
+					printf(\"%s\t%s\t$name\t%s\n\",matches[i,1],matches[i,3],matches[i,2]);
+			};"
+	)"
+
+	if [ "$conf_quick_search" != 'true' ]; then
+
+		eval "find $paths -type f -name '*.html'" 2>/dev/null | \
+		awk -F '/' \
+			"BEGIN {
+				IGNORECASE=1;
+				OFS=\"\t\"
+				count=0;
+			};
+			{
+
+				hits = \$NF;
+				gsub(/^.*:/,\"\",hits);
+
+				gsub(/:[0-9]+$/,\"\",\$0);
+
+				title = \"\";
+				for (i=$nf+2; i<=NF; i++) {
+					title = title ((i==$nf+2) ? \"\" : \"/\") \$i;
+				}
+
+				gsub(/\.html$/,\"\",title);
+				gsub(\"_\",\" \",title);
+				gsub(\"-\",\" \",title);
+
+				title = title \" (\" \$($nf+1) \")\"
+
+				lang=\$$nf;
+				path=\$0;
+
+				matched = title;
+				gsub(/$rg_query/,\"\",matched);
+
+				lm = length(matched)
+				gsub(\" \",\"\",matched);
+				
+				if (length(matched)==0)
+					accuracy = length(title)*100;
+				else
+					accuracy = 100-lm*100/length(title);
+
+				if (accuracy > 0 || book ~ /$rg_query/) {
+					matches[count,0] = accuracy;
+					matches[count,1] = title;
+					matches[count,2] = path;
+					matches[count,3] = lang;
+					count++;
+				}
+			};
+			END {
+				for (i = 0; i < count; i++)
+					for (j = i; j < count; j++)
+						if (matches[i,0] < matches[j,0]) {
+							h = matches[i,0];
+							t = matches[i,1];
+							p = matches[i,2];
+							l = matches[i,3];
+							matches[i,0] = matches[j,0];
+							matches[i,1] = matches[j,1];
+							matches[i,2] = matches[j,2];
+							matches[i,3] = matches[j,3];
+							matches[j,0] = h;
+							matches[j,1] = t;
+							matches[j,2] = p;
+							matches[j,3] = l;
+						};
+						
+				for (i = 0; i < count; i++)
+					printf(\"%s\t%s\t$name\t%s\n\",matches[i,1],matches[i,3],matches[i,2]);
+			};"
+
+	fi
+
+	printf '%s\n%s\n' "$results_title" "$results_text" | awk '!seen[$0] && NF>0 {print} {++seen[$0]};'
 
 }
 

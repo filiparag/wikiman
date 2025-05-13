3aaf21f655cbbc672a75cdcda1c8dac482b970c8
Filip Parag <filip@parag.rs>
2025-05-13 21:37:38 +0200

    strip freebsd docs pages and fix asset symlinks

--- build/sources/fbsd.sh
+++ build/sources/fbsd.sh
@@ -3,7 +3,15 @@
 export XZ_OPT=-e9T0
 
 echo 'Installing tools'
-pacman -Sy --noconfirm rdfind wget
+pacman -Sy --noconfirm rdfind wget git base-devel moreutils
+
+echo 'Building tools'
+git clone --depth 1 https://aur.archlinux.org/pup-bin.git ./pup
+cd ./pup || exit 1
+chown -R nobody .
+chage -E -1 nobody
+su nobody -s /bin/sh -c 'makepkg -sc --noconfirm'
+pacman -U --noconfirm ./pup-bin-*.pkg.tar.zst || exit 1
 
 mkdir -p ./fbsd
 cd ./fbsd || exit 1
@@ -27,9 +35,22 @@ mkdir -p "$dir/usr/share/doc"
 mv "$dir/doc" "$dir/usr/share/doc/freebsd-docs"
 
 echo 'Deduplicating assets'
-rdfind -makehardlinks false -makesymlinks true -makeresultsfile false "$dir/usr/share/doc/freebsd-docs"
+cd "$dir/usr/share/doc/freebsd-docs" || exit 1
+rdfind \
+    -makehardlinks false \
+    -makesymlinks true \
+    -makeresultsfile false \
+    .
+find . -type l | while read -r l; do
+    target="$(realpath "$l")"
+    ln -fs "$(realpath --relative-to="$(dirname "$(realpath -s "$l")")" "$target")" "$l"
+done
+
+echo 'Page post-processing'
+find "$dir/usr/share/doc/freebsd-docs" -type f -name '*.html' -print0 | xargs -0 --no-run-if-empty -I{} sh -c "cat \"{}\" | pup 'head, nav#TableOfContents, div.book, div.article' --pre | sponge \"{}\""
 
 echo 'Compressing data'
+cd "$dir" || exit 1
 archive="freebsd-docs_$(date +'%Y%m%d').source.tar.xz"
 tar -cJf "/release/$archive" usr/share/doc/freebsd-docs
 echo "Generated $(du -h "/release/$archive" | cut -f1) FreeBSD docs archive"
